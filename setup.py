5f0c48e24138eee153e5b77a02856a10ba34bcdb
jinhakim-python <56976518+jinhakim-python@users.noreply.github.com>
2019-10-24 14:21:24 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+APP = ['MSPAINT.py']
+DATA_FILES = []
+OPTIONS = {
+ 'iconfile':'logoapp.icns',
+ 'argv_emulation': True,
+ 'packages': ['certifi'],
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
